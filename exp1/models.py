1aa69f3e31bf378e211a095d19a1d2140f6cbe03
JustinMWoodum <justin.woodum@gmail.com>
2015-10-27 15:15:59 -0700

    .

--- exp1/models.py
+++ exp1/models.py
@@ -5,15 +5,15 @@ class Persona(models.Model):
 	# values = models.ManyToManyField('value') # This will come later.
 
 class Prompt(models.Model):
-	content = CharField()
+	content = CharField(max_length=None)
 	
 class Statement(models.Model):
 	prompt = models.ForeignKey('Prompt')
-	message = CharField()
+	message = CharField(max_length=None)
 	source = URLField(max_length=600)
 	timestamp = DateField(auto_now=False)
-	meaningcloud_sentiment_request = CharField()
-	meaningcloud_sentiment_response = CharField()
+	meaningcloud_sentiment_request = CharField(max_length=None)
+	meaningcloud_sentiment_response = CharField(max_length=None)
 	
 '''
 This will come later.
