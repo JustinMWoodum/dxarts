262c3bd11ed89321dfdb959338b2134536aea25c
JustinMWoodum <justin.woodum@gmail.com>
2015-10-27 16:11:31 -0700

    .

--- exp1/admin.py
+++ exp1/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from models import Prompt, Statement, Persona
 
+class PromptAdmin(admin.ModelAdmin):
+	pass	
+	
 admin.site.register(Prompt)
+
+class StatementAdmin(admin.ModelAdmin):
+	pass
+
 admin.site.register(Statement)
+
+class PersonaAdmin(admin.ModelAdmin):
+	pass
+
 admin.site.register(Persona)
+
