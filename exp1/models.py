3edb636401e18e311ffb853f5ec4fc55097df3b2
JustinMWoodum <justin.woodum@gmail.com>
2015-10-27 15:08:41 -0700

    Created models needed for basic persona generation.

--- exp1/models.py
+++ exp1/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+	statements = models.ManyToManyField('Statement')
+	# values = models.ManyToManyField('value') # This will come later.
+
+class Prompt(models.Model):
+	content = CharField()
+	
+class Statement(models.Model):
+	prompt = models.ForeignKey('Prompt')
+	message = CharField()
+	source = URLField(max_length=600)
+	timestamp = DateField(auto_now=False)
+	meaningcloud_sentiment_request = CharField()
+	meaningcloud_sentiment_response = CharField()
+	
+'''
+This will come later.
+
+class Value(models.Model):
+	# object = ???
+	# sentiment = ???
+
+class Agenda(models.Model):
+	prompts = models.ManyToManyField('prompt')
+'''
