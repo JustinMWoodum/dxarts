192b8e60da5b3f812203f4eb1db909ab1c6bade2
JustinMWoodum <justin.woodum@gmail.com>
2015-10-27 16:00:43 -0700

    .

--- exp1/admin.py
+++ exp1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django_project.exp1.models import Prompt, Statement, Persona
+from models import Prompt, Statement, Persona
 
 admin.site.register(Prompt)
 admin.site.register(Statement)
